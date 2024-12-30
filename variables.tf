33636711183094b9fea522890b9dbd0f0a941c59
01amruta <amrutakate56@gmail.com>
2024-12-30 19:23:56 +0530

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,17 @@
-variable "key-name" {
+# variables.tf
 
+# Variable to define the AWS region
+variable "aws_region" {
+  description = "The AWS region to create resources in"
+  type        = string
+  default     = "ap-south-1"  
 }
 
-variable "network-security-group-name" {
-
+# Variable to define the AMI ID for the EC2 instances
+variable "ami_id" {
+  description = "The AMI ID for EC2 instances"
+  type        = string
+  default     = "ami-08048b042dd26bddb"  
 }
 
-variable "debian-ami" {
 
-}
-
-variable "debian-instance-type" {
-
-}
